07ee4f31786324fb3c8f70e0c749ba99c070c075
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-05-08 16:01:10 -0400

    First Commit

---
+++ Week01/w01_helloworld.py
@@ -0,0 +1,10 @@
+# --------------------------------------
+# Title:     Clint's First Program
+# Author:    Clint MacDonald
+# Date:      May 8, 2025
+# Purpose:   To learn our first Python program
+# --------------------------------------
+
+# Display the message "Hello, World" to the user!
+print("Hello World!")
+  
