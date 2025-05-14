891f46a98da333286546a91e175cfab629b04451
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-05-14 09:48:28 -0400

    Week 2 - Demo 1

---
+++ Week02/WhoAmI.py
@@ -0,0 +1,20 @@
+# -------------------------------------
+# Title:    Who Am I example
+# Author:   Clint MacDonald
+# Date:     May 12, 2025
+# Purpose:  First example of user input
+# -------------------------------------
+
+# Prompt User for their name, and wait for the user to respond
+print('Enter your name: ')
+
+# get the input, and store in defined variable (variant)
+name = input()
+
+# Retrieve value from the variable
+# Say Hello Back using their name
+print("Hello, " + name + "!")
+print("first line\nsecond line")
+
+myNum = 4
+myNum2 = 4.3
