b9a84e0a57faf5d4564f25812f9b5b9272d2da72
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-07-04 10:43:37 -0400

    gfcsg

--- Week08-05/01_ArrayBasic.py
+++ Week08-05/01_ArrayBasic.py
@@ -142,6 +142,49 @@ for i in range(array_length):
     print("%7i - %i" % (i, myArray9[i]))
 
 
+#endregion
 
+#region Array Methods
+# ---------------------------------------------------
+'''
+append()    Adds an element at the end of the list
+clear()     Removes all the elements from the list
+copy()      Returns a copy of the list
+count()     Returns the number of elements with the specified value
+extend()    Add the elements of a list (or any iterable), to the end of the current list
+index()     Returns the index of the first element with the specified value
+insert()    Adds an element at the specified position
+pop()       Removes the element at the specified position
+remove()    Removes the first item with the specified value
+reverse()   Reverses the order of the list
+sort()      Sorts the list
+len()       Returns the number of elements in the list
+list()      Returns a list, i.e. converts a tuple into a list
+tuple()     Returns a tuple, i.e. converts a list into a tuple
+min()       Returns the smallest element in the list
+max()       Returns the largest element in the list
+'''
+'''
+sum()
+'''
+
+# ---------------------------------------------------
+#endregion
+
+# reason to copy - 
+
+a = 5
+b = 6
+# in order to flip these - make a triangle
+c = a
+a = b
+b = c
+
+
+#region powers of 2
+print('-'*40)
+
+for i in range(32):
+    print('%3i - %12i' % (i, 2**i))
 
 #endregion
---
+++ Week08-05/02_GolfSimulator.py
@@ -0,0 +1,19 @@
+# -------------------------
+# COSC1100 - Week 8 Sample 2
+# Clint MacDonald
+# July 4, 2025
+# Golf Simulator to learn to use arrays in practice
+# -------------------------
+
+#region IMPORTS
+#endregion
+
+#region DECLARATIONS
+
+#region CONSTANTS
+#endregion
+
+#region LOCAL VARIABLES
+#endregion
+
+#endregion
